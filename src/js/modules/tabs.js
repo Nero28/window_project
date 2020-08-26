925be72f4604b26283c8157cd0f05f5670d270ee
Nero28 <shackiy.s@yandex.ru>
2020-08-26 14:52:51 +0300

    add hide and show for tabs

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,42 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+    //скрывает весь контент
+    const hideTabContent = () => {
+        content.forEach(item => {
+            item.style.display = 'none';
+        })
+
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        })
+    };
+
+    //показывает определенный контент
+    const showTabContent = (i = 0) => {
+        content[i].style.display = 'block';
+        tab[i].classList.add(activeClass);
+    };
+
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target &&
+            (target.classList.contains(tabSelector.replace(/\./, '')) ||
+                target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
+            tab.forEach((item, i) => {
+                //если выбранный эл. совпадает с перебираемым в массиве
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    })
+};
+
+export default tabs;
